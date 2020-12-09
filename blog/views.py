cd6d9fa52c1a8189cf00c5b341bef63c60a4b16a
chilmnchig <yusuke_wakamoto@yahoo.co.jp>
2020-12-09 15:10:11 +0900

    create paginator

--- blog/views.py
+++ blog/views.py
@@ -8,11 +8,27 @@ from django.utils import timezone
 from blog.models import Blog
 from blog.forms import BlogForm
 
+import math
+
 
 def blog_list(request):
     blogs = Blog.objects.filter(is_public=True, published_at__lte=timezone.now()).order_by('-published_at').all()
+    paginator = Paginator(blogs, per_page=5)
+
+    page = request.GET.get('page')
+    try:
+        blogs = paginator.page(page)
+    except (EmptyPage, PageNotAnInteger):
+        blogs = paginator.page(1)
     for blog in blogs:
         blog.info_content()
+    if blogs.number <= 5:
+        blogs.pages = range(1, min(10, paginator.num_pages) + 1)
+    elif blogs.number >= paginator.num_pages - 4:
+        blogs.pages = range(max(1, paginator.num_pages - 9), paginator.num_pages + 1)
+    else:
+        blogs.pages = range(blogs.number - 5, blogs.number + 5)
+    blogs.last = paginator.num_pages
     return TemplateResponse(request, 'blog/list.html', {'blogs': blogs})
 
 
