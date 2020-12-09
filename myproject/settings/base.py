055beaab951f07f45d63a2f50f10b5a4588e5017
chilmnchig <yusuke_wakamoto@yahoo.co.jp>
2020-12-09 16:28:45 +0900

    change requirement.txt

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_boost',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_boost'
 ]
 
 X_FRAME_OPTIONS = 'ALLOW'
