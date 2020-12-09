02b3a3cfe7084fdcfce14a792f659db4a2f62303
chilmnchig <yusuke_wakamoto@yahoo.co.jp>
2020-12-09 16:32:08 +0900

    change settings.py

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_boost'
+    'django_boost',
 ]
 
 X_FRAME_OPTIONS = 'ALLOW'
